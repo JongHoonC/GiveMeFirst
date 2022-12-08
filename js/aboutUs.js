678e7a48ba5e716a38530c15ac7a753b22d911b6
cheongyeong <cheongyeonggg@gmail.com>
2022-12-08 13:56:44 +0900

    aboutUs 수정

---
+++ js/aboutUs.js
@@ -0,0 +1,10 @@
+function show() {
+  document.querySelector(".factory-adress").className = "factory-adress show";
+}
+document.querySelector("#show").addEventListener("click", show);
+
+// function close() {
+//   document.querySelector(".factory-adress").className = "factory-adress";
+// }
+
+// document.querySelector("#close").addEventListener("click", close);
