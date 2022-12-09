d7c7cc2da50fdad560f35a332f878fd9c9e62e5b
Eddy <cshyeok24@gmail.com>
2022-12-09 12:59:27 +0900

    라우터 설정

--- express/bin/www.js
+++ express/bin/www.js
@@ -1,5 +1,5 @@
-let app = require("../app");
-const port = process.env.PORT || 3000;
+let app = require('../app');
+const port = process.env.PORT || 11111;
 
 app.listen(port, () => {
   console.log(`${port} 로 express 실행`);
--- express/routes/route.js
+++ express/routes/route.js
@@ -3,5 +3,44 @@ const router = express.Router();
 const path = require('path');
 const {send} = require('process');
 
+router.get('/', (req, res) => {
+  res.render('index', {fullpages: true});
+});
+
+router.get('/aboutUs', (req, res) => {
+  res.render('aboutUs', {fullpages: false});
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', {fullpages: false});
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', {fullpages: false});
+});
+
+router.get('/manu', (req, res) => {
+  res.render('Manufacture', {fullpages: false});
+});
+
+router.get('/edit', (req, res) => {
+  res.render('notice_edit', {fullpages: false});
+});
+
+router.get('/noticeDetail', (req, res) => {
+  res.render('notice_page', {fullpages: false});
+});
+
+router.get('/noticeWrite', (req, res) => {
+  res.render('notice_write', {fullpages: false});
+});
+
+router.get('/notice', (req, res) => {
+  res.render('notice', {fullpages: false});
+});
+
+router.get('/productDetail', (req, res) => {
+  res.render('product_detail', {fullpages: false});
+});
 
 module.exports = router;
