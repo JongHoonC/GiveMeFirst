c6fc92aebe83460e743be887ed22f0758b30a355
cheongyeong <cheongyeonggg@gmail.com>
2022-12-14 11:38:31 +0900

    aboutus js

--- express/public/js/aboutUs.js
+++ express/public/js/aboutUs.js
@@ -1,10 +1,10 @@
 $('.popupVideo a').click(function () {
-  $('.video-popup').addClass('reveal'),
-    $('.video-popup .video-wrapper').remove(),
-    $('.video-popup').append(
-      "<div class='video-wrapper'><iframe width='560' height='315' src='https://youtube.com/embed/" + $(this).data('video') + "?rel=0&playsinline=1&autoplay=1' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>",
-    );
-}),
+    $('.video-popup').addClass('reveal'),
+      $('.video-popup .video-wrapper').remove(),
+      $('.video-popup').append(
+        "<div class='video-wrapper'><iframe width='560' height='315' src='https://youtube.com/embed/" + $(this).data('video') + "?rel=0&playsinline=1&autoplay=1' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>",
+      );
+  }),
   $('.video-popup-closer').click(function () {
     $('.video-popup .video-wrapper').remove(), $('.video-popup').removeClass('reveal');
   });
@@ -27,16 +27,27 @@ scrollLink.forEach(link => {
 
     const id = e.currentTarget.getAttribute('href');
 
-    const sec = document.querySelector(id);
+    const sec = document.querySelectorAll('.aboutUs-event')
+    for (let j = 0; j < sec.length; j++) {
+      sec[j].addEventListener('click', () => {
 
-    const secTop = sec.offsetTop;
+      })
+    }
+  });
 
-    window.scrollTo({
-      left: 0,
-      top: secTop,
-    });
+  const secTop = sec.offsetTop;
+
+  window.scrollTo({
+    left: 0,
+    top: secTop + 200,
   });
 });
+
+
+
+
+
+
 
 let factory = document.querySelectorAll('.factory-title');
 let adress = document.querySelectorAll('.factory-adress');
