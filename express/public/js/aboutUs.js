2cb4f1d6c281cf5e0072ff1f03df08e8f085f55b
cheongyeong <cheongyeonggg@gmail.com>
2022-12-14 12:21:26 +0900

    해결 :)

--- express/public/js/aboutUs.js
+++ express/public/js/aboutUs.js
@@ -1,12 +1,12 @@
 //비디오팝업
 
 $('.popupVideo a').click(function () {
-  $('.video-popup').addClass('reveal'),
-    $('.video-popup .video-wrapper').remove(),
-    $('.video-popup').append(
-      "<div class='video-wrapper'><iframe width='560' height='315' src='https://youtube.com/embed/" + $(this).data('video') + "?rel=0&playsinline=1&autoplay=1' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>",
-    );
-}),
+    $('.video-popup').addClass('reveal'),
+      $('.video-popup .video-wrapper').remove(),
+      $('.video-popup').append(
+        "<div class='video-wrapper'><iframe width='560' height='315' src='https://youtube.com/embed/" + $(this).data('video') + "?rel=0&playsinline=1&autoplay=1' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>",
+      );
+  }),
   $('.video-popup-closer').click(function () {
     $('.video-popup .video-wrapper').remove(), $('.video-popup').removeClass('reveal');
   });
@@ -35,19 +35,21 @@ let scrollLink = document.querySelectorAll('.subNav');
 scrollLink.forEach(link => {
   link.addEventListener('click', e => {
     e.preventDefault();
-
-    const id = e.currentTarget.getAttribute('href');
-
-    const sec = document.querySelectorAll('.aboutUs-event');
-    for (let j = 0; j < sec.length; j++) {
-      sec[j].addEventListener('click', () => {});
-    }
-  });
-
-  const secTop = sec.offsetTop;
-
-  window.scrollTo({
-    left: 0,
-    top: secTop + 100,
-  });
+  })
 });
+
+//     const id = e.currentTarget.getAttribute('href');
+
+//     const sec = document.querySelectorAll('.aboutUs-event');
+//     for (let j = 0; j < sec.length; j++) {
+//       sec[j].addEventListener('click', () => {});
+//     }
+//   });
+
+//   const secTop = sec.offsetTop;
+
+//   window.scrollTo({
+//     left: 0,
+//     top: secTop + 100,
+//   });
+// });
