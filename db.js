f38176949079d45f94799042f37512423762ecec
Eddy <cshyeok24@gmail.com>
2022-12-27 13:01:34 +0900

    필요없는 주석제거

--- db.js
+++ db.js
@@ -14,7 +14,7 @@ function getMemo(callback) {
     callback(rows);
   });
 }
-// 작성페이지에서 작성할때 데이터베이스에 정보가 들어가게해주는 함수
+
 function insertMemo(title, content, callback) {
   connection.query(`INSERT INTO noticeTable(create_time,title,content) VALUES (NOW(),'${title}','${content}')`, err => {
     if (err) throw err;
@@ -36,7 +36,6 @@ function getMemoById(id, callback) {
   });
 }
 
-// 공지사항 삭제버튼 누를때 쓰는함수
 function deleteById(id, callback) {
   connection.query(`DELETE FROM noticeTable WHERE id = ${id}`, err => {
     if (err) throw err;
