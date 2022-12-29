73924850ed39357b70c8609b2d12235519a78042
JongHoonC <bjhlove0126@naver.com>
2022-12-29 09:40:21 +0900

    asdfasf

--- public/js/index.js
+++ public/js/index.js
@@ -102,3 +102,19 @@ window.onclick = function (event) {
     }
   }
 };
+
+function filter() {
+  var value, name, item, i;
+
+  value = document.getElementById('value').value.toUpperCase();
+  item = document.getElementsByClassName('list');
+
+  for (i = 0; i < item.length; i++) {
+    name = item[i].getElementsByClassName('title');
+    if (name[0].innerHTML.toUpperCase().indexOf(value) > -1) {
+      item[i].style.display = 'flex';
+    } else {
+      item[i].style.display = 'none';
+    }
+  }
+}
