e33b75007031d918cd76ac12c75420724610c756
JongHoonC <bjhlove0126@naver.com>
2022-12-29 09:48:08 +0900

    검색 기능 구현 완료

--- public/js/index.js
+++ public/js/index.js
@@ -112,6 +112,21 @@ function filter() {
   for (i = 0; i < item.length; i++) {
     name = item[i].getElementsByClassName('title');
     if (name[0].innerHTML.toUpperCase().indexOf(value) > -1) {
+      item[i].style.display = 'flex';
+    } else {
+      item[i].style.display = 'none';
+    }
+  }
+}
+function Nfilter() {
+  var value, name, item, i;
+
+  value = document.getElementById('value').value.toUpperCase();
+  item = document.getElementsByClassName('list');
+
+  for (i = 0; i < item.length; i++) {
+    name = item[i].getElementsByClassName('title');
+    if (name[0].innerHTML.toUpperCase().indexOf(value) > -1) {
       item[i].style.display = 'block';
     } else {
       item[i].style.display = 'none';
