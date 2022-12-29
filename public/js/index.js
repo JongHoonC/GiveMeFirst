49ee88c20e409646132d7ed3b5872e40677e31a7
Eddy <cshyeok24@gmail.com>
2022-12-29 09:45:19 +0900

    검색기능 해결

--- public/js/index.js
+++ public/js/index.js
@@ -112,7 +112,7 @@ function filter() {
   for (i = 0; i < item.length; i++) {
     name = item[i].getElementsByClassName('title');
     if (name[0].innerHTML.toUpperCase().indexOf(value) > -1) {
-      item[i].style.display = 'flex';
+      item[i].style.display = 'block';
     } else {
       item[i].style.display = 'none';
     }
